25d9150796dc74054945df2745f428d59fa461a1
Carlos12001 <carlos.andres12001@gmail.com>
2024-05-01 19:26:23 -0600

    feat: program assembly

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -6,28 +6,14 @@ filename=${1:-main.s}
 # Extrae el nombre base sin la extensión (asumiendo extensión .s)
 base=${filename%.*}
 
-# Compila, enlaza y genera la salida del desensamblador usando el nombre base
-#!/bin/bash
-
-# Asigna el primer argumento a la variable 'filename', si no se proporciona, usa 'main.s'
-filename=${1:-main.s}
-
-port=${2:-12345}
-
-# Extrae el nombre base sin la extensión (asumiendo extensión .s)
-base=${filename%.*}
-
-
-# Detiene el script en caso de errores
-set -e
-# Asegura que los errores en pipelines sean capturados
-set -o pipefail
+# Especifica el puerto para la conexión de depuración
+port=8585
 
 # Ensambla el archivo fuente
-arm-linux-gnueabi-as $base.s -o $base.o
+arm-linux-gnueabi-as -g $filename -o $base.o
 
 # Enlaza el objeto para crear el ejecutable
-arm-linux-gnueabi-ld $base.o -o $base
+arm-linux-gnueabi-ld -g $base.o -o $base
 
 # Crea un dump de desensamblado
 arm-linux-gnueabi-objdump -d $base &> $base.disassembly
